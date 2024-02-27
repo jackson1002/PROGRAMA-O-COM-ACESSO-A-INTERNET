249eb0b0bc5ebc77b573cc59a6060d742d25387e
jackson1002 <102551009+jackson1002@users.noreply.github.com>
2024-02-27 16:18:45 -0300

    ATUALIZEI

---
+++ AULA1/sintaxe.js
@@ -0,0 +1,10 @@
+let nome = 'Tereza'; //string
+let idade = 28; //int
+
+const altura = 1.62; //(const) valor constante
+
+let bol = true; //boolean
+
+let lista = ["banana",'maçã']; //list
+
+let objeto = {nome:'Maria',idade:18}; //objeto
