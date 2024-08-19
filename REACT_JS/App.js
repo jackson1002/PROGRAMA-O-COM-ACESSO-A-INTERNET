f0f73662598de8039fd587f45b88d88153e7ca89
jackson1002 <102551009+jackson1002@users.noreply.github.com>
2024-08-19 16:07:53 -0300

    atualizei

---
+++ REACT_JS/App.js
@@ -0,0 +1,17 @@
+
+/*PRECISA DE NODE PRA EXECUTAR O CÓDIGO ABAIXO E FUNCIONAR NO NOSSO HTML*/
+
+/*function MyButton(){
+    return(
+        <button>Meu botão</button>
+    );
+}
+
+export default function MyApp(){
+    return(
+        <div>
+            <h1>Olá</h1>
+            <MyButton/>
+        </div>
+    )
+}*/
